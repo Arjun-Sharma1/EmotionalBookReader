c35190aca3dd7a8e64f55ee61314394e18018a62
aayush <aayush.bahendwar@ibm.com>
2018-07-12 15:29:28 -0400

    Can grab user input from text box

--- main.py
+++ main.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+
+
+@app.route('/', methods=['POST'])
+def input_text_post():
+    inputText = request.form['inputText']
+    # Put conversion methods here and use inputText as the parameter
+    print(inputText) # Remove this
+    return ('', 204)
+
 if __name__ == "__main__":
+    app.debug = True
     app.run()
