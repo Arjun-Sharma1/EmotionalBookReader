1a2835c2302de7ff56502ea6d97cf91659c844cd
janamanoharan <jana.manoharan96@gmail.com>
2018-07-13 11:36:42 -0400

    done mvp

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,14 +1,29 @@
 from watson_developer_cloud import TextToSpeechV1
-
+import TextToTone as tone
 text_to_speech = TextToSpeechV1(
     username='469c306e-f6d0-4d31-87b3-f603817fb40d',
     password='DOr8056Wp22g'
 )
-words = ["hello", "hi", "jana"]
 
-def text_to_speech():
-    with open('hello_world.wav', 'wb') as audio_file:
+
+words1 = ["<express-as type='GoodNews'>I love you!</express-as>",
+            "<express-as type='Anger'>I hate you!</express-as>",
+                "<express-as type='Anger'>I hate everyone!</express-as>",
+                    "<express-as type='GoodNews'>No wait, I love everyone!</express-as>"]
+words2 = ["I love you!",
+            "I hate you!",
+                "I am so sorry",
+                    "No wait, I love everyone!"]
+def convert_text_to_speech(words):
+    with open('hello_world123.wav', 'wb') as audio_file:
         for word in words:
             audio_file.write(
                 text_to_speech.synthesize(
                     word, 'audio/wav', 'en-US_AllisonVoice').content)
+
+
+if __name__ == "__main__":
+    # convert_text_to_speech(words)
+    words = tone.getTones("I love you! Hello World. I hate you!")
+    convert_text_to_speech(words)
+    # convert_text_to_speech(words3)
--- TextToTone.py
+++ TextToTone.py
@@ -14,33 +14,41 @@ check_sentences = True
 
 def getTones(text):
     tone_dict = tone_analyzer.tone({"text": text}, content_type, check_sentences)
+    print(tone_dict)
     sentences_with_tones = []
     sentences = []
     tones = []
     i = 0
     if text != "":
         if "sentences_tone" in tone_dict:
-            sentences.append(tone_dict.get("sentences_tone"))
-        else:
-            sentences.append(tone_dict.get("document_tone"))
-        for sentence in sentences:
-            if len(sentence.get("tones")) == 0:
-                tone = ""
-            else:
-                tone = sentence.get("tones")[0].get("tone_id")
-                if tone in {"anger", "disgust", "fear", "conscientiousness", "emotional range"}:
-                    tone = "Uncertainty"
-                elif tone == "sadness":
-                    tone = "Apology"
+            sentences = tone_dict.get("sentences_tone")
+            for sentence in sentences:
+                #print("-----sentences-------------")
+                #print(sentences)
+                #print("-----sentence-------------")
+                #print(sentence)
+                if len(sentence.get("tones")) == 0:
+                    tone = ""
                 else:
-                    tone = "Good News"
-            tones.append(tone)
-        for sentence in nltk.sent_tokenize(text):
-            if tones[i] == "":
-                sentences_with_tones.append(sentence)
-            else:
-                sentences_with_tones.append("<express-as type=" + tones[i] + ">" + sentence + "</express-as>")
+                    tone = sentence.get("tones")[0].get("tone_id")
+                    if tone in {"anger", "disgust", "fear", "consciousness", "emotionalRange"}:
+                        tone = "Uncertainty"
+                    elif tone == "sadness":
+                        tone = "Apology"
+                    else:
+                        tone = "GoodNews"
+                tones.append(tone)
+            print(tones)
+            print(nltk.sent_tokenize(text))
+            for sentence in nltk.sent_tokenize(text):
+                if tones[i] == "":
+                    sentences_with_tones.append(sentence)
+                else:
+                    sentences_with_tones.append("<speak><express-as type=\"" + tones[i] + "\">" + sentence + "</express-as></speak>")
                 i += 1
+        else:
+            sentences_with_tones = [text]
+    print(sentences_with_tones)
     return sentences_with_tones
 
 if __name__ == '__main__':
